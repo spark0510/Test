d84708f6ec860120aaed885941af3dd07ff2984b
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2024-01-11 15:06:22 -0500

    sungjae's change2

--- test_functions.R
+++ test_functions.R
@@ -3,6 +3,7 @@
 TestF <- function(faasr) {
   # tbd
   # tbd
+  setwd("FAASR")
 } 
 
 F1 <- function(faasr) {
@@ -42,12 +43,21 @@ F2 <- function(faasr) {
   faasr_log(faasr, "User function F2 finished")
 }	
 
+F2.5 <- function(faasr){
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+  faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
+
+  print("TESTTESTTEST")
+}
+
 F3 <- function(faasr) {
   # print("Hello from inside function Fs")
   args <- faasr_get_user_function_args(faasr)
   # return arguments for this function (F3)
   # expects input1, input2: input file names; output: 
   faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+
+  print("TESTTESTTESTTEST")
   faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
   # need to implement output <- input * input
   # if there's a header in each csv file, "header=T"
